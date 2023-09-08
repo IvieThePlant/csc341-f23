a1853a0cfb1f52650c791f5a6ee45395f882d27f
IvieThePlant <ivietheplant@outlook.com>
2023-09-08 13:42:29 -0500

    first_commit

--- programming-csc341f23/prog00-cyber-csc341f23/Database.java
+++ programming-csc341f23/prog00-cyber-csc341f23/Database.java
@@ -1,20 +1,23 @@
 import java.util.ArrayList;
 import java.io.File;
 import java.util.Scanner;
+
 public class Database {
 
 	Hasher hasher = null;
 
 	private ArrayList<Pair> usernames;
 	private ArrayList<Student> students;
-	
+
 	public Database(Hasher h) {
 		hasher = h;
 		usernames = loadUsernames();
 		students = loadStudents();
 	}
-	
-	/** Checks the given username and password against the usernames ArrayList
+
+	/**
+	 * Checks the given username and password against the usernames ArrayList
+	 * 
 	 * @param username Username of logging in user
 	 * @param password Password(unhashed) to be checked
 	 * @return True if valid, else False
@@ -23,16 +26,16 @@ public class Database {
 		// Find the username in the ArrayList.
 		int x = 0;
 		boolean userfound = false;
-		while (x < usernames.size() &&  !userfound) {
+		while (x < usernames.size() && !userfound) {
 			if (username.equals(usernames.get(x).get(1))) {
 				userfound = true;
 			} else {
-				x ++;
+				x++;
 			}
 		}
 
-		// If the username is valid, hash the password entered by the user 
-		//(using the hash method in Hasher). If not, return false.
+		// If the username is valid, hash the password entered by the user
+		// (using the hash method in Hasher). If not, return false.
 		if (x < usernames.size()) {
 			// Compare the newly hashed password against the one stored in the ArrayList.
 			// If they match, credentials are valid, return true. Else return false.
@@ -43,50 +46,54 @@ public class Database {
 
 		return false;
 	}
-	
-	/** Open users file and load contents. 
+
+	/**
+	 * Open users file and load contents.
+	 * 
 	 * @return ArrayList of username and hashed password pairs from users.txt
-	*/
+	 */
 	public ArrayList<Pair> loadUsernames() {
 		// Open users.txt file -- must use try-catch
-		try{
+		try {
 			File file = new File("users.txt");
 
 			ArrayList<Pair> loadedUsers = new ArrayList<Pair>();
 			Scanner scanner = new Scanner(file);
 
-			while(scanner.hasNextLine()){
+			while (scanner.hasNextLine()) {
 				// Put each line into new Pair with user value1 and pwd value2
 				String[] data = scanner.nextLine().split(",", 8);
-				Pair pair = new Pair(data[0],data[1]);
+				Pair pair = new Pair(data[0], data[1]);
 
 				// Store in new array
 				loadedUsers.add(pair);
-			} 
+			}
 			scanner.close();
 			// return new array
 			return loadedUsers;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return null;
-    }
+	}
 
-	/** Open students file and load contents. 
+	/**
+	 * Open students file and load contents.
+	 * 
 	 * @return Arraylist of all Student objects from students.csv
-	*/
+	 */
 	public ArrayList<Student> loadStudents() {
 		// Open students.csv -- must use try-catch
-		try{
+		try {
 			File file = new File("students.csv");
 			Scanner scanner = new Scanner(file);
 
 			// Put each line into new Student and store in array.
 			ArrayList<Student> loadedStudents = new ArrayList<>();
-			while(scanner.hasNextLine()){
+			while (scanner.hasNextLine()) {
 				String[] dataLine = scanner.nextLine().split(",");
 				ArrayList<String> classes = new ArrayList<>();
-				for (int x = 3; x < dataLine.length; x ++) {
+				for (int x = 3; x < dataLine.length; x++) {
 					classes.add(dataLine[x]);
 				}
 				Student daStudent = new Student(dataLine[0], dataLine[1], dataLine[2], classes);
@@ -95,11 +102,11 @@ public class Database {
 			scanner.close();
 			// return new array
 			return loadedStudents;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 		return null;
-		
+
 	}
 
 	/**
@@ -108,11 +115,11 @@ public class Database {
 	public Student student(String username) {
 		int x = 0;
 		boolean userfound = false;
-		while (x < students.size() &&  !userfound) {
+		while (x < students.size() && !userfound) {
 			if (username.equals(students.get(x).username())) {
 				userfound = true;
 			} else {
-				x ++;
+				x++;
 			}
 		}
 		if (x < students.size()) {
