2f88affc4df1bfdb2208225e4a1be6b5cc292e33
IvieThePlant <ivietheplant@outlook.com>
2023-10-04 13:27:11 -0500

    Finished In Class Comparator

--- exercises-csc341f23/Comparator/Course.java
+++ exercises-csc341f23/Comparator/Course.java
@@ -1,4 +1,4 @@
-public class Course {
+public class Course implements Comparable<Course>{
 	private String dept;
 	private Integer number;
 	private String description;
@@ -11,9 +11,40 @@ public class Course {
 	
 	@Override
 	public String toString() {
-		return dept+number+" "+description;
+		return dept+number+" ";
 	}
 	
 	public String dept() { return dept; }
 	public Integer number() { return number; }
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Course) {
+			Course oCourse = (Course) o;
+			if (this.dept.equals(oCourse.dept)) {
+				if (this.number == oCourse.number) {
+					if (this.description.equals(oCourse.description)){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int compareTo(Course o) {
+		Course oCourse = (Course) o;
+		int res = this.dept.compareTo(oCourse.dept);
+		if (res == 0) {
+			res = this.number.compareTo(oCourse.number);
+			if (res == 0) {
+				return this.description.compareTo(oCourse.description);
+			} else {
+				return res;
+			}
+		} else {
+			return res;
+		}
+	}
 }
