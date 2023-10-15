3bf8bcf8ecd9dc4c57943670be2331b2c46f9ba7
IvieThePlant <ivietheplant@outlook.com>
2023-10-14 21:10:37 -0500

    making sure it doent dry

---
+++ programming-csc341f23/prog04-sort-csc341f23/Prog04Test.java
@@ -0,0 +1,49 @@
+public class Prog04Test {
+
+    // TODO: Write non-default Comparators
+
+    public static void main(String[] args) {
+        // TODO: Create starting list of students
+        Student s1 = new Student("u01");
+        Student s2 = new Student("u02");
+        Student s3 = new Student("u03");
+        Student s4 = new Student("u04");
+        Student s5 = new Student("u05");
+        Student s6 = new Student("u06");
+        Student s7 = new Student("u07");
+        Student s8 = new Student("u08");
+
+        Student sA = new Student("u10");
+        Student sZ = new Student("u11");
+
+        s1.name("Spike");
+        s2.name("Jet");
+        s3.name("Ein");
+        s4.name("Faye");
+        s5.name("Edward");
+
+        sA.name("Amy");
+        sZ.name("Zzazz");
+
+        // sout.print("Recursive Binary Search Method")
+        // TODO: Test findRecurse(Student)
+
+        // sout.print("Ordering Methods")
+        // TODO: Test reorder(Comparator<Student>)
+
+        // TODO: Test min()
+
+        // TODO: Test max()
+
+        // TODO: Test min(Comparator<Student>)
+
+        // TODO: Test max(Comparator<Student>)
+
+        // sout.print("Convert Methods")
+        // TODO: Test toArray(Comparator<Student>)
+
+        // TODO: Test findAll(String)
+    }
+}
+
+// TODO: ADD JAVADOCS
