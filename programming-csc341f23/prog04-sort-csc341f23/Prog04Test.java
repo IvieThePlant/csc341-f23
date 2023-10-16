92b01b6a92ee7c3e57756e1f8a1b91e0c7f0ee5f
IvieThePlant <ivietheplant@outlook.com>
2023-10-16 14:43:53 -0500

    almost done with 4

--- programming-csc341f23/prog04-sort-csc341f23/Prog04Test.java
+++ programming-csc341f23/prog04-sort-csc341f23/Prog04Test.java
@@ -1,6 +1,33 @@
+import java.util.Comparator;
+
 public class Prog04Test {
 
     // TODO: Write non-default Comparators
+    public static Comparator<Student> byName = new Comparator<Student>() {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.name().compareTo(s2.name());
+        }
+    };
+
+    public static Comparator<Student> byUser = new Comparator<Student>() {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.compareTo(s2);
+        }
+    };
+
+    public static Comparator<Student> byMajor = new Comparator<Student>() {
+        @Override
+        public int compare(Student s1, Student s2) {
+            int compairison = s1.major().compareTo(s2.major());
+            if (compairison != 0) {
+                return compairison;
+            } else {
+                return s1.compareTo(s2);
+            }
+        }
+    };
 
     public static void main(String[] args) {
         // TODO: Create starting list of students
@@ -21,25 +48,57 @@ public class Prog04Test {
         s3.name("Ein");
         s4.name("Faye");
         s5.name("Edward");
+        s6.name("Vicious");
+        s7.name("MaoYenrai");
+        s8.name("Julia");
 
-        sA.name("Amy");
-        sZ.name("Zzazz");
+        sA.name("AbdulHakim");
+        sZ.name("TeddyBomber");
 
-        // sout.print("Recursive Binary Search Method")
+        System.out.println("_________________________RECURSIVE BINARY SEARCH METHOD");
         // TODO: Test findRecurse(Student)
+        StudentList listA = new StudentList(8);
+        listA.add(s1);
+        listA.add(s2);
+        listA.add(s3);
+        listA.add(s4);
+        listA.add(s5);
+        listA.add(s6);
+        listA.add(s7);
+        listA.add(s8);
+        Assert.assertEquals(0, listA.findRecurse(s1), "Failed to find s1 - Spike  at listA.students[0]");
+        Assert.assertEquals(3, listA.findRecurse(s4), "Failed to find s4 - Faye   at listA.studnets[3]");
+        Assert.assertEquals(4, listA.findRecurse(s5), "Failed to find s5 - Edward at listA.students[4]");
+        Assert.assertEquals(7, listA.findRecurse(s8), "Failed to find s8 - Julia  at listA.students[7]");
+        Assert.assertEquals(-1, listA.findRecurse(sZ),
+                "Failed to handle looking for non-existant student (expected -1)");
 
-        // sout.print("Ordering Methods")
+        System.out.println("\n_________________________REORDER");
         // TODO: Test reorder(Comparator<Student>)
+        listA.reorder(byName);
+        System.out.println("* Reordered listA by name");
 
+        System.out.println(listA);
+
+        Assert.assertEquals(0, listA.findRecurse(s5), "Failed to find s5 - Edward at listA.students[0]");
+
+        System.out.println("\n_________________________MIN AND MAX");
+        System.out.println(" - looking in listA sorted by name");
         // TODO: Test min()
+        Assert.assertEquals("u05", listA.min().username(), "Incorrect minnimum. Expected s5 - Edward");
 
         // TODO: Test max()
+        Assert.assertEquals("u06", listA.max().username(), "Incorrect minnimum. Expected s6 - Vicious");
+
+        System.out.println(" - Passing Comparator<Student> to find min and max by username");
 
         // TODO: Test min(Comparator<Student>)
+        Assert.assertEquals("u01", listA.min(byUser).username(), "Incorrect minnimum. Expected s1 - Spike");
 
         // TODO: Test max(Comparator<Student>)
+        Assert.assertEquals("u08", listA.max(byUser).username(), "Incorrect minnimum. Expected s8 - Julia");
 
-        // sout.print("Convert Methods")
+        System.out.println("\n_________________________CONVERT METHODS");
         // TODO: Test toArray(Comparator<Student>)
 
         // TODO: Test findAll(String)
