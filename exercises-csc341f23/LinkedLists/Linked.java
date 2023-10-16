5bfa18ebfd968b041ea04fda685b90134456834e
IvieThePlant <ivietheplant@outlook.com>
2023-10-16 16:38:47 -0500

    finished linked exercise

--- exercises-csc341f23/LinkedLists/Linked.java
+++ exercises-csc341f23/LinkedLists/Linked.java
@@ -1,35 +1,56 @@
 public class Linked {
 
     /** Reference to the first element of the list */
-
+    Node head;
     /** Reference to the last element of the list */
+    Node tail;
 
     /** Default constructor */
     public Linked() {
-
+        head = null;
+        tail = null;
     }
 
     /** Empty? */
     public boolean isEmpty() {
-        return true;
+        return (head == null);
     }
 
     @Override
     public String toString() {
+        return toString(head);
+    }
 
-        String list = "";
-
-        return list;
+    private String toString(Node n) {
+        if (n == tail) {
+            return n.toString();
+        } else {
+            return n.toString() + toString(n.next);
+        }
     }
 
     /** add to the front of the list */
     public void prepend(String item) {
-
+        Node newNode = new Node(item);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
     }
 
     /** add to then end of the list */
     public void append(String item) {
-
+        Node newNode = new Node(item);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
     }
 
     private class Node {
@@ -44,6 +65,10 @@ public class Linked {
         Node(String v) {
             value = v;
             next = null;
+        }
+
+        public String toString() {
+            return value;
         }
     }
 }
