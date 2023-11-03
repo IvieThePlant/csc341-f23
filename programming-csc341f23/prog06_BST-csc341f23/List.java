a04c7c5730d8e5d17775c19af497129dfffa91dc
IvieThePlant <ivietheplant@outlook.com>
2023-11-02 19:47:01 -0500

    almost done

--- programming-csc341f23/prog06_BST-csc341f23/List.java
+++ programming-csc341f23/prog06_BST-csc341f23/List.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class List {
 
-	/** Primary entry point into the tree */
-	Node root = null;
+    /** Primary entry point into the tree */
+    Node root = null;
 
     /** Count of elements in the List */
     int count = 0;
@@ -16,11 +16,14 @@ class List {
         }
     };
 
-    /** Sentinal for leaf nodes. This node should be both the left and right child of all leaf nodes */
+    /**
+     * Sentinal for leaf nodes. This node should be both the left and right child of
+     * all leaf nodes
+     */
     Node dummy = new Node(null);
 
     public List() {
-    
+        // TODO: write default constructor
     }
 
     public List(Comparator<Simple> order) {
@@ -28,53 +31,237 @@ class List {
     }
 
     @Override
+    /**
+     * Returns space separated values of the whole tree, in order
+     */
     public String toString() {
-        return null;
+        return toString(root);
+    } // end toString()
 
-    }
-  
+    /**
+     * Private recursive function for toString()
+     * 
+     * @param n Node at recursive step
+     * @return String of all values to the left, own value, and all values to the
+     *         right
+     */
+    private String toString(Node n) {
+        if (n == dummy) {
+            return " ";
+        }
+        return toString(n.left) + n.value + toString(n.right);
+    } // end toString(n)
+
+    /**
+     * Adds new Node with value to the tree, according to Comparator
+     * 
+     * @param simple Value to add to tree
+     */
     public void add(Simple simple) {
-    
-    }
-    
+        // Create new node
+        Node newNode = new Node(simple);
+
+        // If list empty, set as new head
+        if (isEmpty()) {
+            root = newNode;
+            count++;
+            return;
+        }
+
+        // Otherwise, look through list until you are at dummy, then add to one before
+        Node pointer = root;
+        Node setter = null;
+        int comparison = 0;
+        while (pointer != dummy) {
+            setter = root;
+            comparison = ordering.compare(newNode.value, pointer.value);
+            if (comparison < 0) {
+                pointer = pointer.left;
+            } else {
+                pointer = pointer.right;
+            }
+        }
+
+        if (comparison < 0) {
+            setter.left = newNode;
+        } else {
+            setter.right = newNode;
+        }
+        count++;
+    } // end add(simple)
+
+    /**
+     * Finds node with first simple with any matching value
+     * 
+     * @param simple Simple to compare with
+     * @return Simple that has at least 1 matching parameter, else null
+     */
     public Simple get(Simple simple) {
+        // Search through tree, returning matching value, or null if reach dummy
+        Node pointer = root;
+        while (pointer != dummy) {
+            int comparison = ordering.compare(simple, root.value);
+            if (comparison == 0) {
+                return pointer.value;
+            }
+            if (comparison < 0) {
+                pointer = pointer.left;
+            }
+            if (comparison > 0) {
+                pointer = pointer.right;
+            }
+        }
         return null;
-    }
-    
+    } // end get(simple)
+
+    /**
+     * Finds node with first simple with matching string value
+     * 
+     * @param value String to search for
+     * @return Simple that has matching string value, else null
+     */
     public Simple find(String value) {
-    	return null;
-    }
-    
+        // Search through tree, returning matching value, or null if reach dummy
+        Node pointer = root;
+        while (pointer != dummy) {
+            int comparison = value.compareTo(root.value.alpha);
+            if (comparison == 0) {
+                return pointer.value;
+            }
+            if (comparison < 0) {
+                pointer = pointer.left;
+            }
+            if (comparison > 0) {
+                pointer = pointer.right;
+            }
+        }
+        return null;
+    } // end find(value)
+
+    /**
+     * Finds node with first simple with matching Integer value
+     * 
+     * @param value Integer to search for
+     * @return Simple that has matching Integer value, else null
+     */
     public Simple find(Integer value) {
-    	return null;
-    }
-    
+        // Search through tree, returning matching value, or null if reach dummy
+        Node pointer = root;
+        while (pointer != dummy) {
+            int comparison = value - root.value.number;
+            if (comparison == 0) {
+                return pointer.value;
+            }
+            if (comparison < 0) {
+                pointer = pointer.left;
+            }
+            if (comparison > 0) {
+                pointer = pointer.right;
+            }
+        }
+        return null;
+    } // end find(value)
+
+    /**
+     * Finds the minnimum value in list
+     * 
+     * @return Smallest Simple stored in list
+     */
     public Simple min() {
-        return null;
-    }
+        // Go as far left as possible, and return
+        Node pointer = root;
+        while (root.left != dummy) {
+            pointer = pointer.left;
+        }
+        return pointer.value;
+    } // end min()
 
+    /**
+     * Finds the maxximum value in list
+     * 
+     * @return Biggest Simple stored in list
+     */
     public Simple max() {
-        return null;
-    }
+        // Go as far right as possible, and return
+        Node pointer = root;
+        while (root.right != dummy) {
+            pointer = pointer.right;
+        }
+        return pointer.value;
+    } // end max()
 
-    Simple[] toArray() {
-        return null;
+    /**
+     * Checks if list is empty
+     * 
+     * @return True if empty, else false
+     */
+    public boolean isEmpty() {
+        return count == 0;
+    } // end isEmpty()
+
+    /**
+     * Converts the tree into an array, sorted by Comparator
+     * 
+     * @return Simple array of all values in list, sorted
+     */
+    public Simple[] toArray() {
+        // TODO: write toArray()
+        Simple[] newArray = new Simple[count];
+        toArray(newArray, 0, root);
+        return toArray();
+    } // end toArray()
+
+    private int toArray(Simple[] newArray, int added, Node n) {
+        // Base case
+        if (n == dummy) {
+            return added;
+        }
+
+        // Add all Nodes to the left of Node to array
+        added = toArray(newArray, added, n.left);
+
+        // Add Node to array
+
+        // Add all Nodes to the right of Node to array
+
+        return added;
     }
 
     class Node {
-    	Simple value;
-    	Node left;
-    	Node right;
+        // Node's stored value
+        Simple value;
+        // Node's left child node(lesser)
+        Node left;
+        // Node's right child node(greater)
+        Node right;
 
-    	Node(Simple t) {
-    		value = t;
-    	}
-    	
-    	void print() {
-    		System.out.print(value);
-			System.out.print(" (L: "+left.value+") ");
-    		System.out.println(" (R: "+right.value+")");
-    	}
+        /**
+         * Node default constructor
+         */
+        Node() {
+            value = null;
+            left = dummy;
+            right = dummy;
+        }
+
+        /**
+         * Node constructor with given value
+         * 
+         * @param t Value to be stored in node
+         */
+        Node(Simple t) {
+            value = t;
+            left = dummy;
+            right = dummy;
+        }
+
+        /**
+         * Prints out node value, left child's value, and right child's value
+         */
+        void print() {
+            System.out.print(value);
+            System.out.print(" (L: " + left.value + ") ");
+            System.out.println(" (R: " + right.value + ")");
+        }
     } // end class Node
 } // end class List
-
--- programming-csc341f23/prog06_BST-csc341f23/README.md
+++ programming-csc341f23/prog06_BST-csc341f23/README.md
@@ -1,10 +1,11 @@
-## Programming Assignem 6 - Binary Search Trees
+# Programming Assignem 6 - Binary Search Trees
 
 DUE Thursday, November2 (Moodle link will stay open until Friday end-of-day)
 
 In this lab, you will again create the Abstract Data Type List, except this time you will use a Binary Search Tree for implementation. The List will be storing elements of type Simple.
 
 Learning Outcomes:
+
 - Understand the structure of a binary tree.
 - Understand the structure of a binary search tree.
 - Know how to implement a binary search tree.
@@ -14,55 +15,39 @@ Learning Outcomes:
 
 > If you are searching for an element based on the ordering of the tree, then you can use binary search. However, if you need to access every element in the tree or perform a "linear" search, you must use recursion. This is the ONLY way to traverse the tree (unless you maintain your own stack, which we will not do until a few weeks from now).
 
-
-<hr>
-
-
-### General Requirements
+## General Requirements
 
 Note that in this implementation, you will be using a "dummy" or sentinal node to mark the bottom of the tree. Use the SAME dummy node as both the left and right child of all leaf nodes.
-
-
 
 1. `class Node`. Add a default constructor to the Node class.
 2. `class Node`. Modify the constructor so that each child points to the dummy node.
 
-1. Implementation for class List
-- `void add(Simple value)`
+3. Implementation for class List
+    - `void add(Simple value)`
 
-- `String toString()` : Create a string of values in order
+    - `String toString()` : Create a string of values in order
 
-- `Simple get(Simple value)` : Use List ordering to find first match. Note that it will only match on the basis of the ordering. 
+    - `Simple get(Simple value)` : Use List ordering to find first match. Note that it will only match on the basis of the ordering.
 
-- `Simple find(String value)`: Use Binary Search to locate an element with the specified value. Return null if not in the List.
+    - `Simple find(String value)`: Use Binary Search to locate an element with the specified value. Return null if not in the List.
 
-- `Simple find(Integer value)`: Use Binary Search to locate an element with the specified value. Return null if not in the List.
+    - `Simple find(Integer value)`: Use Binary Search to locate an element with the specified value. Return null if not in the List.
 
-- `Simple min()` : based on order of tree
+    - `Simple min()` : based on order of tree
 
-- `Simple max()` : based on order of tree
+    - `Simple max()` : based on order of tree
 
-- `Simple[] toArray()` : place elements in the tree into the array IN SORTED ORDER. You cannot use any Java functions to do this for you. You must declare the array, and individually place each element into the array. **This is a challenging method to write! You have to keep track of where the next element is to be added. It will have to be passed around as you traverse the tree (recursively).
+    - `Simple[] toArray()` : place elements in the tree into the array IN SORTED ORDER. You cannot use any Java functions to do this for you. You must declare the array, and individually place each element into the array. **This is a challenging method to write! You have to keep track of where the next element is to be added. It will have to be passed around as you traverse the tree (recursively).
 
-1. Javadocs: complete and polished for `class List`.
-1. Code is commented in all methods of `class List`.
-1. Code is style compliant.
+4. Javadocs: complete and polished for `class List`.
+
+5. Code is commented in all methods of `class List`.
+
+6. Code is style compliant.
 
 It is your responsibility to make sure your code is working.
 
-<hr>
+### Resources
 
-### Resources 
-
-- https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html
+- <https://docs.oracle.com/javase/8/docs/api/java/util/Comparator.html>
 - zyBooks reading assignment #07
-
-
-
-
-
-
-
-
-
-
