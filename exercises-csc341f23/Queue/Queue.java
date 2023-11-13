dd6a5b248bb2173d2a83cfa4a67c6596e844b084
IvieThePlant <ivietheplant@outlook.com>
2023-11-13 15:00:58 -0600

    Queue

---
+++ exercises-csc341f23/Queue/Queue.java
@@ -0,0 +1,39 @@
+class Queue<T> {
+    T[] queue;
+    int head;
+    int tail;
+
+    public Queue(int capacity) {
+        @SuppressWarnings("unchecked")
+        T[] temp = (T[]) new Object[capacity];
+        queue = temp;
+    }
+
+    private boolean isFull() {
+        return (head == tail) && (queue[head] != null);
+    }
+
+    private boolean isEmpty() {
+        return (head == tail) && (queue[head] == null);
+    }
+
+    public boolean push(T thing) {
+        if (isFull()) {
+            return false;
+        } else {
+            queue[tail] = thing;
+            tail = (tail + 1) % queue.length;
+            return true;
+        }
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        } else {
+            T temp = queue[head];
+            head = (head + 1) % queue.length;
+            return temp;
+        }
+    }
+}
